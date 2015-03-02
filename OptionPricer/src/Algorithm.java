25398db42804e1274f653ec8059356305d5c7e68
sky-xu <sky.xutianyu@gmail.com>
2015-03-02 14:21:10 +1030

    initial upload 20150302 14:20
    create the project

---
+++ OptionPricer/src/Algorithm.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Sky on 2015/3/2-002.
+ */
+public class Algorithm {
+    double[] results;
+    public final int NUMOFDOTS=11;
+    abstract public double[] computeOption(Option o);
+    abstract public double crunchPut(Option o);
+    abstract public double crunchCall(Option o);
+}
---
+++ OptionPricer/src/BlackScholes.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Sky on 2015/3/2-002.
+ */
+public class BlackScholes extends Algorithm{
+
+}
